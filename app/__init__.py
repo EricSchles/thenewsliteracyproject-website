04ba7641c62046b47bc9a4d971edca2396dfa436
Eric Schles <ericschles@gmail.com>
2017-01-13 22:58:48 -0500

    adding markdown rendering support

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from flaskext.markdown import Markdown
 from .commands import REPL
 import os
 
@@ -16,5 +17,6 @@ migrate = Migrate(app,db)
 manager = Manager(app)
 manager.add_command('db',MigrateCommand)
 manager.add_command("shell",REPL())
+markdown = Markdown(app)
 
 from app import views, models
