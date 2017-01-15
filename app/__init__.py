7fe2a9d7c138f7d650de8cd38c1dca1d9f87512c
Eric Schles <ericschles@gmail.com>
2017-01-15 14:57:46 -0500

    updating

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,9 @@ import os
 app = Flask(__name__)
 username="news_literacy_admin"
 password="1234"
-#app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "testing"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://"+username+":"+password+"@localhost/news_literacy_db"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://"+username+":"+password+"@localhost/news_literacy_db"
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 manager = Manager(app)
